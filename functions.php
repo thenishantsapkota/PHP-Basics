9f16a04f8cea854379bb81714077de754d20aa14
Nishant Sapkota <snishant306@gmail.com>
2022-09-17 10:28:05 +0545

    feat: functions

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+    // Functions
+    function sayHello($name){
+        echo "Hello there, $name"."<br />";
+    }
+    sayHello("Nishant");
+
+    function addNumbers($num1 = 10, $num2 = 20){
+        return $num1 + $num2;
+    }
+
+    function formatBlog($blog){
+        echo "{$blog['title']}'s author is {$blog['author']} and it has {$blog['likes']} likes. <br />";
+    }
+
+    formatBlog(["title" => "Nishant's Blog", "author" => "nishant", "likes" => 20]);
+    $sum = addNumbers();
+    echo $sum;
+?>
